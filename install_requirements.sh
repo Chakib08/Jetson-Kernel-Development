c81bf238d2def4324316fb2221c84dc06fb738f9
Mohamed Lamine Kartobi <chakib08.chakibdace@gmail.com>
2023-04-10 17:19:30 +0200

    Add tegra sources download

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-CROSS_COMPILE_PATH=$HOME/l4t-gcc/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/
+# Export jetson nano jetpack utils paths
+. ./utils.sh
 
+ERR=0
+
+# Linux4Tegra sources version is 32.7.3 by default
+if [ -z $1 ]
+then
+    L4T_VERSION=32.7.3
+else
+    L4T_VERSION=$1
+fi
 
 # Download GCC Linaro toolchain to cross-compile Jetson's image
 if ! test -e $CROSS_COMPILE_PATH;
@@ -15,3 +25,21 @@ else
     echo "GCC Linaro toolchain already downloaded, skipping..."
 fi
 
+
+if ! test -e $TEGRA_SOURCES
+then
+    cd $JETPACK
+    echo "Downloading TEGRA sources from tag tegra-l4t-r$L4T_VERSION"
+    ./source_sync.sh -t tegra-l4t-r$L4T_VERSION
+else
+    echo "TEGRA from tag tegra-l4t-r$L4T_VERSION sources already dowloaded, skipping..."
+fi
+
+ERR=$?
+
+if [ $ERR -eq 0 ] 
+then
+    echo "All requirements were installed successfully"
+else
+    echo "ERROR: One or more packages couldn't be installed"
+fi
---
+++ utils.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export CROSS_COMPILE_PATH=$HOME/l4t-gcc/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/
+export JETPACK=$HOME/nvidia/nvidia_sdk/JetPack_4.6.3_Linux_JETSON_NANO_TARGETS/Linux_for_Tegra/
+export TEGRA_SOURCES=$JETPACK/sources/
