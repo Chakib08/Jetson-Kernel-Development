55f45fa1a944d0cde9891eafa0611f18657445d9
Mohamed Lamine Kartobi <chakib08.chakibdace@gmail.com>
2023-04-15 15:11:57 +0200

    Update path

--- utils.sh
+++ utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CROSS_COMPILE_PATH=$HOME/l4t-gcc/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/
-export JETPACK=$HOME/nvidia/nvidia_sdk/JetPack_4.6.3_Linux_JETSON_NANO_TARGETS/Linux_for_Tegra/
+export JETPACK=$HOME/nvidia/nvidia_sdk/JetPack_4.6.3_Linux_JETSON_NANO_TARGETS/Linux_for_Tegra
 export TEGRA_SOURCES=$JETPACK/sources/
 export TOOLCHAIN_PREFIX=$CROSS_COMPILE_PATH/bin/aarch64-linux-gnu-
 export TEGRA_KERNEL_OUT=$JETPACK/build
