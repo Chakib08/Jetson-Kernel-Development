7f81da7fd77741a886cc0b7bc7c3837d291ed431
Mohamed Lamine KARTOBI <chakibdace@gmail.com>
2024-10-13 14:08:17 +0200

    Update copy image and dtb script

--- scripts/copy_img_dtb.sh
+++ scripts/copy_img_dtb.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-# Export jetson nano jetpack utils paths
+# Export Jetson Nano JetPack utils paths
 . ./utils.sh
 
-scp $JETPACK/kernel/Image jetson@192.168.55.1:/home/jetson/
-scp $JETPACK/kernel/dtb/$DTB jetson@192.168.55.1:/home/jetson/
+# Prompt the user for the password securely
+echo "Enter your password for sudo and SSH:"
+read -s PASSWORD
 
-echo "Run in jetson"
+# Copy kernel Image and dtb to Jetson via SCP
+sshpass -p "$PASSWORD" scp "$JETPACK/kernel/Image" jetson@192.168.55.1:/home/jetson/
+
+if [ $? -eq 0 ]; then
+    echo "Image copied from host to Jetson"
+else
+    echo "Password is incorrect."
+    exit 1
+fi
+
+sshpass -p "$PASSWORD" scp "$JETPACK/kernel/dtb/$DTB" jetson@192.168.55.1:/home/jetson/
+echo "DTB copied from host to Jetson"
+
+# Display instructions for manual commands
+echo "Run on Jetson Nano:"
 echo "sudo cp Image /boot/"
 echo "sudo cp tegra210-p3448-0000-p3449-0000-b00.dtb /boot/dtb/"
-ssh jetson@192.168.55.1
 
+# SSH into Jetson with the provided password
+sshpass -p "$PASSWORD" ssh jetson@192.168.55.1
